5c9781b0e6d36fae56e2dd339a85848252692f0e
jasonklein <jasonklein@gmail.com>
2014-05-16 16:40:47 +0100

    Initial commit.

---
+++ stacksy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stacksy::Application.config.secret_token = '6f986a0bf486a3b3c453f832ca754cbe3a3f0ee3a999cee964787a4cb3b444c3558676f3b512c39d777b6654da97d748bde881e4cf485c34566f75c13ab5e497'
