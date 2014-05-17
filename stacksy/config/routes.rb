fd156fa20ce06d9bd70ffb68f3f7f726776e2987
jasonklein <jasonklein@gmail.com>
2014-05-17 15:03:26 +0100

    Configure app to use devise gem, delete default rails index page.

--- stacksy/config/routes.rb
+++ stacksy/config/routes.rb
@@ -1,5 +1,7 @@
 Stacksy::Application.routes.draw do
 
+  devise_for :users
+
 resources :users do
   resources :interests
   resources :blocks
--- stacksy/db/schema.rb
+++ stacksy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140517115337) do
+ActiveRecord::Schema.define(:version => 20140517135024) do
 
   create_table "blocks", :force => true do |t|
     t.integer  "blocker_id"
@@ -38,8 +38,10 @@ ActiveRecord::Schema.define(:version => 20140517115337) do
     t.integer  "sender_id"
     t.integer  "recipient_id"
     t.text     "content"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                              :null => false
+    t.datetime "updated_at",                              :null => false
+    t.boolean  "sender_readability",    :default => true
+    t.boolean  "recipient_readability", :default => true
   end
 
   create_table "pings", :force => true do |t|
@@ -73,15 +75,28 @@ ActiveRecord::Schema.define(:version => 20140517115337) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name",                                      :null => false
-    t.string   "gender",                                    :null => false
-    t.string   "relationship_status", :default => "single", :null => false
-    t.integer  "age",                                       :null => false
-    t.string   "location",                                  :null => false
-    t.string   "role",                :default => "basic",  :null => false
-    t.string   "other",               :default => "",       :null => false
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.string   "name",                                         :null => false
+    t.string   "gender",                                       :null => false
+    t.string   "relationship_status",    :default => "single", :null => false
+    t.integer  "age",                                          :null => false
+    t.string   "location",                                     :null => false
+    t.string   "role",                   :default => "basic",  :null => false
+    t.string   "other",                  :default => "",       :null => false
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
+    t.string   "email",                  :default => "",       :null => false
+    t.string   "encrypted_password",     :default => "",       :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0,        :null => false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
