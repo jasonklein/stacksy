cec085b57fd9ef8eb4ade73cc63bf21b75e4a16d
jasonklein <jasonklein@gmail.com>
2014-05-20 16:01:31 +0100

    Add fade out for javascript sent alert, add validation for presence of message content.

--- stacksy/app/controllers/messages_controller.rb
+++ stacksy/app/controllers/messages_controller.rb
@@ -19,21 +19,15 @@ class MessagesController < ApplicationController
     @message.recipient_id = params[:recipient_id]
     @message.sender_id = current_user.id
 
-    # respond_to do |format|
-    #   format.html # new.html.erb
-    #   format.json {render json: @message}
-    # end
-
-
     respond_to do |format|
       if @message.save
         format.js 
         format.html { redirect_to user_home_path(current_user), notice: 'Sent to #{@message.recipient.name}. Sweet!' }
-        format.json { render json: @message, status: :created, location: @message }
+        # format.json { render json: @message, status: :created, location: @message }
       else
         format.js 
         format.html { render action: "new" }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
+        # format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
