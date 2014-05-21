9e374f0f4cc8d4518021cdb235ac8ec9b272468d
jasonklein <jasonklein@gmail.com>
2014-05-21 21:30:20 +0100

    Undo changes in messages index view related to sender/recipient name migration.

--- stacksy/db/migrate/20140521200124_add_sender_name_and_recipient_name_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddSenderNameAndRecipientNameToMessages < ActiveRecord::Migration
-  def change
-    add_column :messages, :sender_name, :string
-    add_column :messages, :recipient_name, :string
-  end
-end
