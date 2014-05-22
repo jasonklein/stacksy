9b512f2fbee6f2fe1c8eea3214dbd78a5f903824
hansss <hanakim@college.harvard.edu>
2014-05-22 10:39:33 +0100

    running latest migrations

--- stacksy/db/schema.rb
+++ stacksy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140521221527) do
+ActiveRecord::Schema.define(:version => 20140522092726) do
 
   create_table "blocks", :force => true do |t|
     t.integer  "blocker_id"
