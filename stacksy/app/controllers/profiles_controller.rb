22a2bf5b466c6bca3cb5466844bebe664c8f1238
jasonklein <jasonklein@gmail.com>
2014-05-22 22:03:48 +0100

    Increased bottom margin on h1 tag.

--- stacksy/app/controllers/profiles_controller.rb
+++ stacksy/app/controllers/profiles_controller.rb
@@ -2,14 +2,15 @@ class ProfilesController < ApplicationController
 
   
   def show
-    @profile = Profile.find(params[:id])
-    @user = @profile.user
-    if @profile.user != current_user
+    @user = User.find(params[:id])
+    @profile = @user.profile
+    if @user != current_user
       @profile.add_track(current_user)
     end
   end
 
   def edit
+
   end
 
   def update
